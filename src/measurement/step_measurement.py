395824aa288b822a81d78d6a0ea6b1e9442dae7b
Lab User C54-01 <ong-git@phys.ethz.ch>
2023-10-19 10:32:45 +0200

    Step measurement gui and measurement test file

--- src/measurement/step_measurement.py
+++ src/measurement/step_measurement.py
@@ -1,78 +1,199 @@
+import datetime
+import os
 import time
 
+import numpy as np
+from PyQt6.QtCore import pyqtSignal, QObject, QThread
 from onglabsuite.instruments.thorlabs.pm100d import PM100D
 from pymeasure.instruments.attocube import ANC300Controller
 
 import logging
 
+from pymeasure.instruments.attocube.anc300 import Axis
+
 logger = logging.getLogger(__name__)
 
 
-def measure(devices, **kwargs):
-    """
+class StepMeasurement(QObject):
+    newDataPoint = pyqtSignal(float)
+    measurementFinished = pyqtSignal(bool)
 
-    :param devices:
-    :param kwargs:
-    :return:
-    """
-    logger.info("Starting step size measurement")
+    def __init__(self, devices=None):
+        super().__init__()
+        self.devices = devices
+        self.running = True
 
-    # check if necessary devices are available
-    if not devices["anc300"]:
-        raise RuntimeError("No ANC300 device found")
-    # if not devices["pm"]:
-    #     raise RuntimeError("No powermeter device found")
+    def measure(self, devices=None, **kwargs):
+        """
 
-    # get devices
-    logger.info("Getting devices")
+        :param devices:
+        :param kwargs:
+        :return:
+        """
+        if devices is None:
+            devices = self.devices
+            if devices is None:
+                raise RuntimeError("No devices given")
 
-    anc300: ANC300Controller = devices["anc300"]
-    pm: PM100D = devices["pm"]
+        logger.info("Starting step size measurement")
 
-    # parameters
+        # check if necessary devices are available
+        if not devices["anc300"]:
+            raise RuntimeError("No ANC300 device found")
+        if not devices["pm"]:
+            raise RuntimeError("No powermeter device found")
 
-    starting_voltage = 25  # volt
-    delta_voltage = -1  # volt
-    end_voltage = 15
-    max_steps = 100
+        # get devices
+        logger.info("Getting devices")
 
-    save_path = "C:\\Users\\ONG_C62_01\\Documents\\test.csv"
+        anc300: ANC300Controller = devices["anc300"]
+        pm: PM100D = devices["pm"]
 
-    logger.info("Setting up devices")
-    # setup devices
-    anc300.stop_all()
-    anc300.ground_all()
+        # parameters
 
-    anc300.LX.mode = "stp"
+        starting_voltage = 26  # volt
+        delta_voltage = -1  # volt
+        end_voltage = 16
+        power_avg = 5
 
-    # setup
-    logger.info("Starting loop")
-    for voltage in range(
-        starting_voltage,
-        end_voltage,
-        delta_voltage,
-    ):
-        logger.info(f"setting voltage to {voltage}")
-        anc300.LX.voltage = voltage
-        logger.info(f"waiting for 5 seconds")
-        time.sleep(5)
+        powers = np.empty((power_avg, 1))
 
-        logger.info(f"measuring power")
-        # power = pm.measure_power()
-        # logger.info(f"power is {power}")
-        # logger.info(f"writing to file")
-        # with open(save_path, "a") as f:
-        # f.write(f"{voltage},{power}\n")
+        save_path = (
+            r"C:\Users\ONG_C54_01\Documents\MeasurementData\Ueli\StepMeasurement"
+        )
 
-    anc300.LX.stop()
-    anc300.LX.mode = "gnd"
+        logger.info("Setting up devices")
+        # setup devices
+        anc300.stop_all()
+        anc300.ground_all()
+
+        axis: Axis = anc300.LX
+
+        axis.mode = "stp"
+
+        time.sleep(1)
+
+        # setup
+        logger.info("Starting loop")
+        try:
+            for voltage in range(
+                starting_voltage,
+                end_voltage,
+                delta_voltage,
+            ):
+                rep = 0
+                # for rep, voltage in enumerate([starting_voltage] * 10):
+                if not self.running:
+                    logger.info("Measurement stopped")
+                    break
+                logger.info(f"setting voltage to {voltage}")
+                axis.voltage = voltage
+                # anc300.read()
+                logger.debug(f"waiting for 1 seconds")
+                time.sleep(1)
+
+                date = datetime.datetime.now().strftime("%Y%m%d")
+                filename = (
+                    save_path + rf"\{date}_voltage-{voltage}_repetion-{rep+1}_.csv"
+                )
+                while os.path.exists(filename):
+                    if filename.split("_")[-2].isdigit():
+                        file_ext = int(filename.split("_")[-2]) + 1
+                        filename = (
+                            "_".join(filename.split("_")[:-2]) + f"_{file_ext}_.csv"
+                        )
+                    else:
+                        filename = filename[:-4] + f"0_.csv"
+                logger.info(f"filename is {filename}")
+
+                storage = []
+                peak = False
+                i = 0
+                direction = 1
+
+                logger.info("Stepping")
+                while i < 300 or peak or direction == -1:
+                    if not self.running:
+                        logger.info("Stepping stopped")
+                        break
+                    if direction == 1:
+                        axis.stepu = 1
+                    else:
+                        axis.stepd = 1
+                    i += 1
+                    time.sleep(2 / axis.frequency)
+                    logger.debug(f"measuring power")
+                    for n in range(power_avg):
+                        powers[n] = pm.power_W
+                        time.sleep(0.05)
+                    power = np.average(powers)
+                    logger.debug(f"power is {power}")
+
+                    storage.append(power)
+
+                    if len(storage) > 5:
+                        delta = np.sum(np.diff(storage[-5:]))
+                        tot_storage = np.sum(storage[-5:])
+                        logger.debug(f"delta is {delta}")
+                        if delta < 0 and tot_storage > 1e-7:
+                            logger.debug(f"peak detected")
+                            peak = True
+
+                    if direction == 1:
+                        logger.debug(f"writing to file")
+
+                        self.newDataPoint.emit(power)
+                        with open(
+                            filename,
+                            "a",
+                        ) as f:
+                            f.write(f"{i}, step, {power}, watt, {peak}\n")
+
+                    time.sleep(0.1)
+
+                    if peak and power < 1e-8:
+                        # logger.info("stepped over peak")
+                        # break
+                        if direction == 1:
+                            logger.info("stepping back")
+                            direction = -1
+                            peak = False
+                            storage = []
+                            i = 0
+                        else:
+                            logger.info("finished")
+                            break
+                # logger.info(f"stepping back {i} steps")
+                # axis.stepd = i
+                # time.sleep(i / axis.frequency * 1.2)
+                # input("Press enter to continue")
+
+        except Exception as e:
+            print(f"Something went wrong: {e}")
+        finally:
+            logger.info("Disconnecting anc")
+            axis.stop()
+            axis.mode = "gnd"
+            logger.info("Disconnecting powermeter")
+            pm.close()
+            time.sleep(1)
+            logger.info("Measurement finished")
+            self.measurementFinished.emit(True)
 
 
 if __name__ == "__main__":
+    logger.setLevel(logging.INFO)
+    logger.addHandler(logging.StreamHandler())
+    logger.info("Connecting devices")
     controller = ANC300Controller(
         adapter="TCPIP::192.168.1.2::7230::SOCKET",
         axisnames=["RZ", "RY", "RX", "LZ", "LY", "LX"],
         passwd="123456",
     )
-    test_devices = {"anc300": controller, "pm": None}
-    measure(test_devices)
+    logger.info("ANC300 connected")
+    powermeter = PM100D("USB0::0x1313::0x8078::P0021350::INSTR")
+    logger.info("Powermeter connected")
+    test_devices = {"anc300": controller, "pm": powermeter}
+    logger.info("Calling measure")
+    experiment = StepMeasurement()
+    experiment.measure(test_devices)
---
+++ src/measurement/step_measurement_gui.py
@@ -0,0 +1,186 @@
+import datetime
+import os
+import sys
+import random
+from PyQt6.QtCore import Qt, QTimer, QThread, pyqtSlot
+from PyQt6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QVBoxLayout,
+    QWidget,
+    QPushButton,
+    QTextEdit,
+)
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib
+from onglabsuite.instruments.thorlabs.pm100d import PM100D
+from pymeasure.instruments.attocube import ANC300Controller
+
+from src.measurement.step_measurement import StepMeasurement
+
+matplotlib.use("QtAgg")
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class QTextEditHandler(logging.Handler):
+    def __init__(self, text_widget):
+        super().__init__()
+        self.text_widget = text_widget
+
+    def emit(self, record):
+        log_message = self.format(record)
+        self.text_widget.append(log_message)
+
+
+class MeasurementApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Real-Time Measurement Plot")
+        self.setGeometry(100, 100, 800, 600)
+
+        self.central_widget = QWidget(self)
+        self.setCentralWidget(self.central_widget)
+
+        self.layout = QVBoxLayout(self.central_widget)
+
+        self.plot_button = QPushButton("Start Measurement", self)
+        self.layout.addWidget(self.plot_button)
+
+        self.stop_button = QPushButton("Stop Measurement", self)
+        self.layout.addWidget(self.stop_button)
+        self.stop_button.clicked.connect(self.stop_experiment)
+        self.stop_button.setEnabled(False)
+
+        self.canvas = RealTimePlotCanvas(self, width=5, height=4)
+        self.layout.addWidget(self.canvas)
+
+        self.log_text = QTextEdit(self)
+        self.log_text.setReadOnly(True)
+        self.log_text.ensureCursorVisible()
+        self.log_text.verticalScrollBar().rangeChanged.connect(self.change_scroll)
+        self.layout.addWidget(self.log_text)
+
+        self.plot_button.clicked.connect(self.start_measurement)
+
+        self.thread: QThread = None
+        self.experiment: StepMeasurement = None
+        self.data = []
+        # self.timer = QTimer(self)
+        # self.timer.timeout.connect(self.update_plot)
+
+    @pyqtSlot(bool)
+    def switch_start(self, value):
+        self.plot_button.setEnabled(value)
+        self.stop_button.setEnabled(not value)
+
+    @pyqtSlot(int, int)
+    def change_scroll(self, min, max):
+        # print("cambio", min, max)
+        self.log_text.verticalScrollBar().setSliderPosition(max)
+
+    def start_measurement(self):
+        self.plot_button.setEnabled(False)
+        self.stop_button.setEnabled(True)
+        self.data = []  # Reset data
+        # self.timer.start(1000)  # Update plot every 1 second
+        logger.info("Connecting devices")
+        controller = ANC300Controller(
+            adapter="TCPIP::192.168.1.2::7230::SOCKET",
+            axisnames=["RZ", "RY", "RX", "LZ", "LY", "LX"],
+            passwd="123456",
+        )
+        logger.info("ANC300 connected")
+        powermeter = PM100D("USB0::0x1313::0x8078::P0021350::INSTR")
+        devices = {"anc300": controller, "pm": powermeter}
+        logger.info("Powermeter connected")
+        logger.info("Setup Thread")
+        # create a thread
+        self.thread = QThread()
+        logger.debug("Thread created")
+        # create the experiment
+        self.experiment = StepMeasurement(devices)
+        logger.debug("Experiment created")
+        # move experiment to thread
+        self.experiment.moveToThread(self.thread)
+        logger.debug("Experiment moved to thread")
+        # connecting signals
+        self.thread.started.connect(self.experiment.measure)
+        self.experiment.newDataPoint.connect(self.update_plot)
+        self.experiment.measurementFinished.connect(self.switch_start)
+        self.thread.finished.connect(self.thread.deleteLater)
+        self.experiment.measurementFinished.connect(self.experiment.deleteLater)
+        logger.debug("Signals connected")
+
+        # start the thread
+        logger.info("measurement started")
+        self.thread.start()
+        # self.experiment.measure(devices)
+
+    def stop_experiment(self):
+        logger.info("stopping experiment")
+        self.experiment.running = False
+        # self.thread.wait()
+
+    def update_plot(self, new_data_point):  # Simulate a measurement
+        self.data.append(new_data_point)
+        if len(self.data) > 200:
+            self.data = self.data[-200:]
+        self.canvas.update_plot(self.data)
+
+
+class RealTimePlotCanvas(FigureCanvas):
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        super().__init__(fig)
+        self.setParent(parent)
+
+    def update_plot(self, data):
+        self.axes.clear()
+        self.axes.plot(data)
+        self.axes.set_xlabel("Time")
+        self.axes.set_ylabel("Measurement Data")
+        self.axes.grid(True)
+        self.draw()
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MeasurementApp()
+
+    log_handler = QTextEditHandler(window.log_text)
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    log_handler.setFormatter(formatter)
+    log_handler.setLevel(logging.INFO)
+
+    date = datetime.datetime.now().strftime("%Y%m%d")
+    filename = rf"C:\Users\ONG_C54_01\Documents\MeasurementData\Ueli\StepMeasurement\log\{date}_step_measurement_.log"
+    while os.path.exists(filename):
+        if filename.split("_")[-2].isdigit():
+            file_ext = int(filename.split("_")[-2]) + 1
+            filename = "_".join(filename.split("_")[:-2]) + f"_{file_ext}_.log"
+        else:
+            filename = filename[:-4] + f"0_.log"
+    print(f"Log file location is {filename}")
+    file_handler = logging.FileHandler(filename)
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.DEBUG)
+
+    main_logger = logging.getLogger()
+    main_logger.addHandler(log_handler)
+    main_logger.addHandler(file_handler)
+    main_logger.setLevel(logging.DEBUG)
+
+    window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
