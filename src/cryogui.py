4205341af4b6cf34f860a0b066d365d2a83e154e
Lab User C54-01 <ong-git@phys.ethz.ch>
2023-10-25 13:12:36 +0200

    Work on cryogui

--- src/cryogui.py
+++ src/cryogui.py
@@ -16,11 +16,14 @@ from PyQt6.QtWidgets import (
     QComboBox,
     QTextEdit,
     QLineEdit,
+    QHBoxLayout,
+    QSpacerItem,
 )
 
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from pyqtgraph import PlotWidget
+import pyqtgraph as pg
 
 from src.dummies.dummycontroller import DummyAttoDRY
 
@@ -65,6 +68,14 @@ class RealTimePlotCanvas(FigureCanvas):
         self.draw()
 
 
+class ControlPanel(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.gridLayout = QGridLayout(self)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+
+
 class LoggerInterface(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -79,19 +90,43 @@ class LoggerInterface(QMainWindow):
         self.action_monitor = QTextEdit()
         self.is_connected = False
 
-        self.stage_temp_canvas = PlotWidget()
-        self.stage_temp_canvas.plot([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        self.stage_temp_canvas = PlotWidget(title="Sample Temperature")
+        self.stage_pressure_canvas = PlotWidget(title="Sample Space Pressure")
+        self.cold_temp_canvas = PlotWidget(title="Cold Plate Temperature")
+        self.heater_power_canvas = PlotWidget(title="Heater Power")
+        self.turbo_pump_canvas = PlotWidget(title="Turbo Pump Frequency")
 
-        self.stage_pressure_canvas = PlotWidget()
-        self.stage_pressure_canvas.plot([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        self.canvases = [
+            self.stage_temp_canvas,
+            self.stage_pressure_canvas,
+            self.cold_temp_canvas,
+            self.heater_power_canvas,
+            self.turbo_pump_canvas,
+        ]
 
-        self.cold_temp_canvas = PlotWidget()
-        self.cold_temp_canvas.plot([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        self.data_names = [
+            "Temperature / K",
+            "Pressure / mbar",
+            "Temperature / K",
+            "Power / W",
+            "Frequency / Hz",
+        ]
+        self.data_titles = [
+            "Sample Temperature",
+            "Sample Space Pressure",
+            "Cold Plate Temperature",
+            "Heater Power",
+            "Turbo Pump Frequency",
+        ]
 
-        self.heater_power_canvas = PlotWidget()
-        self.heater_power_canvas.plot([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        self.data = []
 
         self.init_ui()
+
+        for widget in self.findChildren(QWidget):
+            if widget == self.connect_button or widget == self.port_combo:
+                continue
+            widget.setEnabled(False)
 
         # self.timer = QTimer(self)
         # self.timer.timeout.connect(self.read_and_log_data)
@@ -101,14 +136,14 @@ class LoggerInterface(QMainWindow):
         self.central_widget = QWidget(self)
         self.setCentralWidget(self.central_widget)
         self.layout = QGridLayout()
+        # self.layout.setContentsMargins(0, 0, 0, 0)
+
         self.central_widget.setLayout(self.layout)
 
         port_label = QLabel("Port")
-        self.layout.addWidget(port_label, 0, 0)
         ports = [port for port in QSerialPortInfo.availablePorts()]
         self.port_combo = QComboBox()
         self.port_combo.addItems([port.portName() for port in ports])
-        self.layout.addWidget(self.port_combo, 0, 1)
 
         self.logging_timer = QTimer(
             self,
@@ -135,12 +170,9 @@ class LoggerInterface(QMainWindow):
             )
         )
         self.logging_interval_edit.setEnabled(False)
-        self.layout.addWidget(self.logging_interval_edit, 2, 2)
-        self.layout.addWidget(QLabel("Logging Interval (ms)"), 2, 1)
 
         self.connect_button = QPushButton("Connect")
         self.connect_button.clicked.connect(self.connect_controller)
-        self.layout.addWidget(self.connect_button, 0, 2)
 
         self.log_file_browse = QPushButton("Start Logging")
         self.file_locator = QLineEdit()
@@ -150,15 +182,78 @@ class LoggerInterface(QMainWindow):
         self.disconnect_button.setEnabled(False)
         self.disconnect_button.clicked.connect(self.disconnect_controller)
 
+        self.action_monitor.setReadOnly(True)
+        # self.layout.setColumnStretch()
+
+        self.layout.addWidget(port_label, 0, 0)
+        self.layout.addWidget(self.port_combo, 0, 1)
+        self.layout.addWidget(self.connect_button, 0, 2)
         self.layout.addWidget(self.disconnect_button, 0, 3)
         self.layout.addWidget(self.file_locator, 1, 0, 1, 3)
         self.layout.addWidget(self.log_file_browse, 1, 3)
-        self.action_monitor.setReadOnly(True)
-        self.layout.addWidget(self.action_monitor, 3, 0, 1, 4)
-        self.layout.addWidget(self.stage_temp_canvas, 4, 0, 1, 4)
-        self.layout.addWidget(self.stage_pressure_canvas, 5, 0, 1, 4)
-        self.layout.addWidget(self.cold_temp_canvas, 6, 0, 1, 4)
-        self.layout.addWidget(self.heater_power_canvas, 7, 0, 1, 4)
+        self.layout.addWidget(QLabel("Logging Interval (ms)"), 2, 1)
+        self.layout.addWidget(self.logging_interval_edit, 2, 2)
+        self.layout.addWidget(self.stage_temp_canvas, 3, 0, 1, 4)
+        self.layout.addWidget(self.stage_pressure_canvas, 4, 0, 1, 4)
+        self.layout.addWidget(self.turbo_pump_canvas, 5, 0, 1, 4)
+        self.layout.addWidget(self.cold_temp_canvas, 3, 4, 1, 4)
+        self.layout.addWidget(self.heater_power_canvas, 4, 4, 1, 4)
+        self.layout.addWidget(self.action_monitor, 5, 4, 1, 4)
+
+        self.horizontalLayout_3 = QHBoxLayout()
+        spacerItem = QSpacerItem(
+            40,
+            20,
+            QtWidgets.QSizePolicy.Policy.Expanding,
+            QtWidgets.QSizePolicy.Policy.Minimum,
+        )
+        self.horizontalLayout_3.addItem(spacerItem)
+        self.pushButton_3 = QPushButton("Confirm")
+        self.horizontalLayout_3.addWidget(self.pushButton_3)
+        spacerItem1 = QSpacerItem(
+            40,
+            20,
+            QtWidgets.QSizePolicy.Policy.Expanding,
+            QtWidgets.QSizePolicy.Policy.Minimum,
+        )
+        self.horizontalLayout_3.addItem(spacerItem1)
+        self.pushButton_2 = QPushButton("Cancel")
+        self.horizontalLayout_3.addWidget(self.pushButton_2)
+        spacerItem2 = QSpacerItem(
+            40,
+            20,
+            QtWidgets.QSizePolicy.Policy.Expanding,
+            QtWidgets.QSizePolicy.Policy.Minimum,
+        )
+        self.horizontalLayout_3.addItem(spacerItem2)
+        self.layout.addLayout(self.horizontalLayout_3, 0, 7, 1, 1)
+        self.pushButton_5 = QPushButton("Activate Temperature control")
+        self.layout.addWidget(self.pushButton_5, 2, 5, 1, 1)
+        self.horizontalLayout = QHBoxLayout()
+        self.pushButton_6 = QPushButton("Set PID")
+        self.horizontalLayout.addWidget(self.pushButton_6)
+        self.lineEdit_2 = QLineEdit(self)
+        self.horizontalLayout.addWidget(self.lineEdit_2)
+        self.lineEdit_3 = QLineEdit(self)
+        self.horizontalLayout.addWidget(self.lineEdit_3)
+        self.lineEdit_4 = QLineEdit(self)
+        self.horizontalLayout.addWidget(self.lineEdit_4)
+        self.layout.addLayout(self.horizontalLayout, 2, 7, 1, 1)
+        self.pushButton_4 = QPushButton("Heat Up")
+        self.layout.addWidget(self.pushButton_4, 1, 5, 1, 1)
+        self.lineEdit = QLineEdit(self)
+        self.layout.addWidget(self.lineEdit, 2, 6, 1, 1)
+        self.pushButton = QPushButton("Base Temperature")
+        self.layout.addWidget(self.pushButton, 0, 5, 1, 1)
+
+        for i, plot_widget in enumerate(self.canvases):
+            plot_widget.showGrid(x=True, y=True, alpha=0.5)
+            plot_widget.getAxis("bottom").setLabel("Time", units="s")
+            plot_widget.getAxis("left").setLabel(self.data_names[i], units="units")
+            plot_widget.getAxis("bottom").setPen(pg.mkPen(color="k"))
+            plot_widget.getAxis("left").setPen(pg.mkPen(color="k"))
+            plot_widget.setBackground("w")  # White background
+            plot_widget.setXRange(0, 1)
 
     def logging_manager(self, running: bool = False):
         if not running:
@@ -225,6 +320,6 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = LoggerInterface()
     window.setWindowTitle("Logger Interface")
-    window.setGeometry(100, 20, 600, 800)
+    window.setGeometry(100, 20, 1500, 800)
     window.show()
     sys.exit(app.exec())
